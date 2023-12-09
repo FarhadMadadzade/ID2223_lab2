3c7b2edbd574cbfc193421a9f0635d388cf8e545
Farhad Madadzade <farhadm1999@gmail.com>
2023-12-09 14:21:08 +0100

    added huggingface interface files

--- huggingface-interface/app.py
+++ huggingface-interface/app.py
@@ -1,60 +1,80 @@
+from transformers import pipeline
 import gradio as gr
-from PIL import Image
-import requests
-import hopsworks
-import joblib
-import pandas as pd
+import time
+from video_downloader import download_video1, download_youtube_video
+from moviepy.editor import AudioFileClip, VideoFileClip
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import datetime
+import os
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import re
 
-project = hopsworks.login()
-fs = project.get_feature_store()
-
-
-mr = project.get_model_registry()
-
-# OLD CODE FROM WINE
-# model = mr.get_model("wine_model", version=1)
-# model_dir = model.download()
-# model = joblib.load(model_dir + "/wine_model.pkl")
-# print("Model downloaded")
+pipe = pipeline("automatic-speech-recognition", model="Artanis1551/whisper_swedish")
 
 
-# def wine(alcohol, volatile_acidity, chlorides, sulphates, free_sulfur_dioxide):
-#     print("Calling function")
-#     df = pd.DataFrame(
-#         [[alcohol, volatile_acidity, chlorides, sulphates, free_sulfur_dioxide]],
-#         columns=[
-#             "alcohol",
-#             "volatile_acidity",
-#             "chlorides",
-#             "sulphates",
-#             "free_sulfur_dioxide",
-#         ],
-#     )
-#     print("Predicting")
-#     print(df)
-#     # 'res' is a list of predictions returned as the label.
-#     res = model.predict(df)
-#     # We add '[0]' to the result of the transformed 'res', because 'res' is a list, and we only want
-#     # the first element.
-#     #     print("Res: {0}").format(res)
-#     print(res)
-#     image = Image.open("./images/" + str(int(res[0])) + "_glass.png")
-#     return image
+def process_video1(date):
+    # If the date is not in YYYY-MM-DD format, return an error message
+    date_pattern = re.compile(r"\b\d{4}-\d{2}-\d{2}\b")
+    if not date_pattern.match(date):
+        video_path = download_youtube_video(
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        )
+        transcription = "Please enter a date in the format YYYY-MM-DD."
+        return video_path, transcription
+    try:
+        video_path = download_video1(date)
+
+        # Get the duration of the video
+        video = VideoFileClip(video_path)
+        duration = video.duration
+
+        # If the video is longer than 30 seconds, only take the first 30 seconds
+        if duration > 30:
+            video_path = f"short_{date}.mp4"
+            ffmpeg_extract_subclip(video_path, 0, 30, targetname=video_path)
+
+        # Extract audio from the video
+        audio_path = f"audio_{date}.wav"
+        AudioFileClip(video_path).write_audiofile(audio_path)
+
+        # Split the audio into chunks
+        audio = AudioSegment.from_wav(audio_path)
+        chunks = split_on_silence(audio, min_silence_len=500, silence_thresh=-40)
+
+        # Transcribe each chunk
+        transcription = ""
+        for i, chunk in enumerate(chunks):
+            chunk.export(f"chunk{i}.wav", format="wav")
+            with open(f"chunk{i}.wav", "rb") as audio_file:
+                audio = audio_file.read()
+            transcription += pipe(audio)["text"] + "\n "
+            os.remove(f"chunk{i}.wav")
+
+        # Remove the audio file
+        os.remove(audio_path)
+    except:
+        video_path = download_youtube_video(
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        )
+        transcription = "No decision was made on this date."
+
+    return video_path, transcription
 
 
-# demo = gr.Interface(
-#     fn=wine,
-#     title="Wine Quality Predictive Analytics",
-#     description="Experiment with alcohol/volatile acidity/chlorides/sulphates/wine type and see the prediction of the wine quality.",
-#     allow_flagging="never",
-#     inputs=[
-#         gr.inputs.Number(default=10.0, label="alcohol"),
-#         gr.inputs.Number(default=0.2, label="volatile acidity"),
-#         gr.inputs.Number(default=0.69, label="sulphates"),
-#         gr.inputs.Number(default=0.03, label="chlorides"),
-#         gr.inputs.Number(default=71, label="free sulfur dioxide"),
-#     ],
-#     outputs=gr.Image(type="pil", label="Wine Quality (1-3)"),
-# )
+iface = gr.Interface(
+    fn=process_video1,
+    inputs=[
+        gr.inputs.Textbox(label="Date with format YYYY-MM-DD"),
+    ],
+    outputs=[
+        gr.outputs.Video(),
+        gr.Textbox(lines=100, max_lines=100, interactive=True),
+    ],
+    title="Transcribe Swedish Parliament Decisions",
+    description="This app transcribes the top Swedish Parliament decision"
+    + " video from the given date. Only the first 30 seconds of the "
+    + "video will be used if it is longer than that.",
+)
 
-# demo.launch(debug=True)
+iface.launch()
